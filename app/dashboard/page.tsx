3ac9793a079b1a83aaea5d9aae85fa6a13115ba9
frako23 <frako23@gmail.com>
2025-10-18 19:06:33 -0400

    get user product information from db and user information from auth 25m

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,30 @@
 import Sidebar from "@/components/sidebar";
+import { getCurrentUser } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const user = await getCurrentUser();
+  const userId = user.id;
+  const totalProducts = await prisma.product.count({ where: { userId } });
+  console.log(totalProducts);
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar currentPath="/dashboard" />
+      <main className="ml-64 p-8">
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Dashboard
+              </h1>
+              <p className="text-sm text-gray-500">
+                Welcome back! Here is an overview fo your inventory
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
